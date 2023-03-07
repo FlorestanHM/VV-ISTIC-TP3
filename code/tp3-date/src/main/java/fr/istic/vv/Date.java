ec219985b07119c7790c5295f4ad8485d2730d32
fhacquesmini <florestan.hacques-minier@etudiant.univ-rennes1.fr>
2023-03-07 14:02:24 +0100

    feat: Date & DateTest

--- code/tp3-date/src/main/java/fr/istic/vv/Date.java
+++ code/tp3-date/src/main/java/fr/istic/vv/Date.java
@@ -1,17 +1,86 @@
 package fr.istic.vv;
 
 class Date implements Comparable<Date> {
+    private final int day;
+    private final int month;
+    private final int year;
 
-    public Date(int day, int month, int year) { }
+    public Date(int day, int month, int year) throws InvalidDateExcepetion {
+        if(isValidDate(day,month,year)) {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        } else {
+            throw new InvalidDateExcepetion();
+        }
+    }
 
-    public static boolean isValidDate(int day, int month, int year) { return false; }
+    private static int numberOfDaysInMonth(int month, int year) {
+        return switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 4, 6, 9, 11 -> 30;
+            case 2 -> isLeapYear(year) ? 29 : 28;
+            default -> -1;
+        };
+    }
 
-    public static boolean isLeapYear(int year) { return false; }
+    public static boolean isValidDate(int day, int month, int year) {
+        return month <= 12 && day > 0 && day <= numberOfDaysInMonth(month, year);
+    }
 
-    public Date nextDate() { return null; }
+    public static boolean isLeapYear(int year) { return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0); }
 
-    public Date previousDate() { return null; }
+    public Date nextDate() {
+        try {
+            if (isValidDate(this.day+1, this.month, this.year)) {
+                return new Date(this.day+1, this.month, this.year);
+            } else if (isValidDate(1, this.month + 1, this.year)) {
+                return new Date(1, this.month+1, this.year);
+            } else {
+                return new Date(1, 1, this.year+1);
+            }
+        }
+        catch (InvalidDateExcepetion e) {
+            return this;
+        }
+    }
 
-    public int compareTo(Date other) { return 0; }
+    public Date previousDate() {
+        try {
+            if (isValidDate(this.day-1, this.month, this.year)) {
+                return new Date(this.day-1, this.month, this.year);
+            } else if (isValidDate(numberOfDaysInMonth(this.month-1, this.year), this.month - 1, this.year)) {
+                return new Date(numberOfDaysInMonth(this.month-1, this.year), this.month - 1, this.year);
+            } else {
+                return new Date(31, 12, this.year-1);
+            }
+        }
+        catch (InvalidDateExcepetion e) {
+            return this;
+        }
+    }
 
+    public int compareTo(Date other) {
+        if (this.year != other.year) {
+            return Integer.compare(this.year, other.year);
+        } else if (this.month != other.month) {
+            return Integer.compare(this.month, other.month);
+        } else {
+            return Integer.compare(this.day, other.day);
+        }
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public static class InvalidDateExcepetion extends Throwable { }
 }
--- code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
+++ code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
@@ -1,10 +1,136 @@
 package fr.istic.vv;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DateTest {
+
+    @ParameterizedTest(name = "{index} => day={0}, month={1}, year={2}")
+    @CsvFileSource(resources = "/validDates.csv")
+    void testPreviousDate(int day, int month, int year) {
+        try {
+            Date date = new Date(day, month, year);
+            Date previousDate = date.previousDate();
+            assertTrue(Date.isValidDate(previousDate.getDay(), previousDate.getMonth(), previousDate.getYear()));
+        } catch (Date.InvalidDateExcepetion invalidDateExcepetion) {
+            fail("La date testée n'est pas valide");
+        }
+    }
+
+    @ParameterizedTest(name = "{index} => day={0}, month={1}, year={2}")
+    @CsvFileSource(resources = "/validDates.csv")
+    void testNextDate(int day, int month, int year) {
+        try {
+            Date date = new Date(day, month, year);
+            Date nextDate = date.nextDate();
+            assertTrue(Date.isValidDate(nextDate.getDay(), nextDate.getMonth(), nextDate.getYear()));
+        } catch (Date.InvalidDateExcepetion invalidDateExcepetion) {
+            fail("La date testée n'est pas valide");
+        }
+    }
+
+    @ParameterizedTest(name = "{index} => day={0}, month={1}, year={2}")
+    @CsvFileSource(resources = "/validDates.csv")
+    void testConstructor(int day, int month, int year) {
+        try {
+            Date date = new Date(day, month, year);
+            assertTrue(Date.isValidDate(day, month, year));
+        } catch (Date.InvalidDateExcepetion invalidDateExcepetion) {
+            assertFalse(Date.isValidDate(day, month, year));
+        }
+    }
+
+    @ParameterizedTest(name = "{index} => day={0}, month={1}, year={2}")
+    @CsvFileSource(resources = "/validDates.csv")
+    void testValidDate(int day, int month, int year) {
+        assertTrue(Date.isValidDate(day, month, year));
+    }
+
+    @ParameterizedTest(name = "{index} => day={0}, month={1}, year={2}")
+    @CsvFileSource(resources = "/invalidDates.csv")
+    void testInvalidDate(int day, int month, int year) {
+        assertFalse(Date.isValidDate(day, month, year));
+        assertThrows(Date.InvalidDateExcepetion.class, () -> new Date(day, month, year));
+    }
+
+    @Test
+    void testPosterieurCompareTo() {
+        try {
+            Date date = new Date(10, 1, 23);
+
+            Date posterieurDay = new Date(11, 1, 23);
+
+            Date posterieurMonth = new Date(9, 2, 23);
+            Date posterieurMonth2 = new Date(10, 2, 23);
+            Date posterieurMonth3 = new Date(12, 2, 23);
+
+            Date posterieurYear0 = new Date(9, 1, 24);
+            Date posterieurYear1 = new Date(10, 1, 24);
+            Date posterieurYear2 = new Date(11, 1, 24);
+
+            Date posterieurYear3 = new Date(9, 2, 24);
+
+            assertTrue(date.compareTo(posterieurDay) < 0);
+
+            assertTrue(date.compareTo(posterieurMonth) < 0);
+            assertTrue(date.compareTo(posterieurMonth2) < 0);
+            assertTrue(date.compareTo(posterieurMonth3) < 0);
+
+            assertTrue(date.compareTo(posterieurYear0) < 0);
+            assertTrue(date.compareTo(posterieurYear1) < 0);
+            assertTrue(date.compareTo(posterieurYear2) < 0);
+            assertTrue(date.compareTo(posterieurYear3) < 0);
+        } catch (Date.InvalidDateExcepetion e) {
+            fail("La date testée n'est pas valide");
+        }
+    }
+
+    @Test
+    void testEqualsCompareTo() {
+        try {
+            Date date = new Date(10, 1, 23);
+            Date other = new Date(10, 1, 23);
+
+            assertEquals(0, date.compareTo(other));
+        } catch (Date.InvalidDateExcepetion e) {
+            fail("La date testée n'est pas valide");
+        }
+    }
+
+    @Test
+    void testAnterieurCompareTo() {
+        try {
+            Date date = new Date(15, 6, 23);
+
+            Date anterieurDay = new Date(14, 6, 23);
+
+            Date anterieurMonth = new Date(16, 5, 23);
+            Date anterieurMonth2 = new Date(15, 5, 23);
+            Date anterieurMonth3 = new Date(14, 5, 23);
+
+            Date anterieurYear0 = new Date(16, 6, 22);
+            Date anterieurYear1 = new Date(15, 6, 22);
+            Date anterieurYear2 = new Date(14, 6, 22);
+
+            Date anterieurYear3 = new Date(16, 8, 22);
+
+            assertTrue(date.compareTo(anterieurDay) > 0);
+
+            assertTrue(date.compareTo(anterieurMonth) > 0);
+            assertTrue(date.compareTo(anterieurMonth2) > 0);
+            assertTrue(date.compareTo(anterieurMonth3) > 0);
+
+            assertTrue(date.compareTo(anterieurYear0) > 0);
+            assertTrue(date.compareTo(anterieurYear1) > 0);
+            assertTrue(date.compareTo(anterieurYear2) > 0);
+            assertTrue(date.compareTo(anterieurYear3) > 0);
+        } catch (Date.InvalidDateExcepetion e) {
+            fail("La date testée n'est pas valide");
+        }
+    }
 
 
 }
