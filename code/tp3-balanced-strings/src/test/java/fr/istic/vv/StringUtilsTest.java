09e7584e418ed4e4f310e955c2492bda8a9257fb
fhacquesmini <florestan.hacques-minier@etudiant.univ-rennes1.fr>
2023-03-07 12:33:30 +0100

    update: balance_strings

--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -4,8 +4,30 @@ import org.junit.jupiter.api.Test;
 
 import static fr.istic.vv.StringUtils.isBalanced;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StringUtilsTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"", "{}", "[]", "()", "{[]}", "([])", "({})", "{()[]}", "([]{})", "[({})]"
+            , "Hello World !"
+            , "{a}", "[a]", "(a)"
+            , "{a[a]}", "(a[a])", "(a{a})", "{a(a)[a]}", "(a[a]{a})"})
+    void testIsBalanced(String input) {
+        assertTrue(isBalanced(input));
+    }
 
+    @ParameterizedTest
+    @ValueSource(strings = { "}", "]", ")", "}]", ")]", "{", "[", "(", "{[", "[(", "{]", "[)", "(}", "{)[]", "([)]"
+            , "{}]", "()[}", "{[])", "{})[]"
+            , "a}", "a]a", ")a", "a}a]", "a)a]"
+            , "a{", "a[", "a(", "a{a[", "a[a("
+            , "{a]", "[a)", "(a}", "{a)[a]", "a(a[a)]"
+            , "a{a}]", "a(a)[a}", "a{a[a])", "a{a}a)a[]"})
+    void testIsNotBalanced(String input) {
+        assertFalse(isBalanced(input));
+    }
 
 }
